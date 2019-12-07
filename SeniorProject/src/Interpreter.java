0c37e68a529e837de29c13736a271463d284570a
Aamir Ibrahim <31293238+AamirIbrahim@users.noreply.github.com>
2019-12-07 16:50:20 -0500

    Added an output to a GUI

--- SeniorProject/src/Interpreter.java
+++ SeniorProject/src/Interpreter.java
@@ -1,41 +1,62 @@
 /*
-Team Members:
+Author:
 Aamir Ibrahim
-Bayleigh Correl
-Adam Gonglach
-Harry Haisty
  */
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.regex.Pattern;
 
-public class Interpreter
-{
+public class Interpreter {
 	public static void main(String[] args) {
 		try {
+			FileTransfer("src/test1.cs", "src/a.java");
+			CSharptoJava("src/a.java");
+			FileTransfer("src/test2.cs", "src/b.java");
+			CSharptoJava("src/b.java");
+			FileTransfer("src/test3.cs", "src/c.java");
+			CSharptoJava("src/c.java");
+			FileTransfer("src/test4.cs", "src/d.java");
+			CSharptoJava("src/d.java");
 
-			System.out.print("First Program Output: \n");
+			PrintStream printStream = new PrintStream(new FileOutputStream("src/output.txt"));
+			System.setOut(printStream);
+
 			Parser p = new Parser("src/test1.cs");
 			Program program1 = p.parse();
 			program1.execute();
 			System.out.println();
+			printStream.close();
 
-			System.out.print("Second Program Output: \n");
+			PrintStream printStream2 = new PrintStream(new FileOutputStream("src/output2.txt"));
+			System.setOut(printStream2);
+
 			Parser p2 = new Parser("src/test2.cs");
 			Program program2 = p2.parse();
 			program2.execute();
 			System.out.println();
+			printStream2.close();
 
-			System.out.print("Third Program Output: \n");
+			PrintStream printStream3 = new PrintStream(new FileOutputStream("src/output3.txt"));
+			System.setOut(printStream3);
+
 			Parser p3 = new Parser("src/test3.cs");
 			Program program3 = p3.parse();
 			program3.execute();
 			System.out.println();
+			printStream3.close();
 
-			System.out.print("Fourth Program Output: \n");
+			PrintStream printStream4 = new PrintStream(new FileOutputStream("src/output4.txt"));
+			System.setOut(printStream4);
+
 			Parser p4 = new Parser("src/test4.cs");
 			Program program4 = p4.parse();
 			program4.execute();
 			System.out.println();
+			printStream4.close();
 
+			interpreterGUI gui = new interpreterGUI("src/test1.cs", "src/a.java", "src/output.txt");
+			interpreterGUI gui2 = new interpreterGUI("src/test2.cs", "src/b.java", "src/output2.txt");
+			interpreterGUI gui3 = new interpreterGUI("src/test3.cs", "src/c.java", "src/output3.txt");
+			interpreterGUI gui4 = new interpreterGUI("src/test4.cs", "src/d.java", "src/output4.txt");
 		}
 		catch (ParserException e) {
 			System.out.println (e.getMessage());
@@ -51,6 +72,54 @@ public class Interpreter
 		}
 		catch (Exception e) {
 			System.out.println ("unknown error occurred - terminating");
+		}
+	}
+	private static void CSharptoJava(String fileName) throws IOException {
+		try {
+			File file = new File(fileName);
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String line = "", oldtext = "";
+			while ((line = reader.readLine()) != null) {
+				oldtext += line + "\r\n";
+			}
+			reader.close();
+
+			String newText = oldtext.replaceAll(Pattern.quote("string[]"), "String[]");
+			newText = newText.replaceAll("Console.WriteLine", "System.out.println");
+			newText = newText.replaceAll("Main", "main");
+
+			FileWriter writer = new FileWriter(fileName);
+			writer.write(newText);
+			writer.close();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	private static void FileTransfer(String inputFile, String outputFile){
+		File file = new File(outputFile);
+		FileInputStream instream = null;
+		FileOutputStream outstream = null;
+		try{
+			File infile = new File(inputFile);
+			File outfile = new File(outputFile);
+
+			instream = new FileInputStream(infile);
+			outstream = new FileOutputStream(outfile);
+
+			byte[] buffer = new byte[1024];
+
+			int length;
+
+			while ((length = instream.read(buffer)) > 0){
+				outstream.write(buffer, 0, length);
+			}
+
+			instream.close();
+			outstream.close();
+
+		}catch(IOException ioe){
+			ioe.printStackTrace();
 		}
 	}
 }
