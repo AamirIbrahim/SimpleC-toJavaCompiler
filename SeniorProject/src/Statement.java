80a265aed1704575f749d97dd97b95a0db00fb63
Aamir Ibrahim <31293238+AamirIbrahim@users.noreply.github.com>
2019-12-07 17:03:26 -0500

    author changes

--- SeniorProject/src/Statement.java
+++ SeniorProject/src/Statement.java
@@ -1,10 +1,7 @@
 /*
-Team Members:
+Author:
 Aamir Ibrahim
-Bayleigh Correl
-Adam Gonglach
-Harry Haisty
- */
+*/
 public interface Statement {
 	// statement has been executed
 	public void execute();
