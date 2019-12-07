18df6793b65a5bfe5ab75da173a0dca77fbff30d
Aamir Ibrahim <31293238+AamirIbrahim@users.noreply.github.com>
2019-12-07 16:52:26 -0500

    author changes

--- SeniorProject/src/ArithmeticExpression.java
+++ SeniorProject/src/ArithmeticExpression.java
@@ -1,10 +1,7 @@
 /*
-Team Members:
+Author:
 Aamir Ibrahim
-Bayleigh Correl
-Adam Gonglach
-Harry Haisty
- */
+*/
 public interface ArithmeticExpression {
 	//return value of the arithmetic expression
 	public int evaluate();
